84d7faef6cb32b55395e633c488c8a4811288c00
Ahmedrh77 <49535160+Ahmedrh77@users.noreply.github.com>
2021-04-03 19:08:48 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 
-app.set("view engine", "ejs");
+app.set('views', './views');
+app.set('view engine', 'ejs')
 
 
 app.use(express.static("public"));
